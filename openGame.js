f864080a6d340517fb2e5b76458ba9b2b25de4b4
Shaurya-Sehgal <102002567+Shaurya-Sehgal@users.noreply.github.com>
2022-05-18 16:53:46 -0600

    Initial Commit

---
+++ openGame.js
@@ -0,0 +1,17 @@
+function opengame(game){
+    if(game == "ticTacToe"){
+        location.href = "https://shaurya-sehgal.github.io/Tic-Tac-Toe/"
+    }
+    else if(game == "carRace"){
+        location.href = "https://shaurya-sehgal.github.io/Car-Race/"
+    }
+    else if(game == "gol"){
+        location.href = "https://shaurya-sehgal.github.io/Game-Of-Luck/"
+    }
+    else if(game == "ticTacToe") {
+        location.href = "https://shaurya-sehgal.github.io/Tic-Tac-Toe/"
+    }
+    else if(game == "CarRace2"){
+        location.href = "https://shaurya-sehgal.github.io/carrace2/"
+    }
+}
