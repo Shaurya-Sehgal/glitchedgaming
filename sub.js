b3483f11c7581c14844e5117627f4adbc7e79f2d
Shaurya-Sehgal <ashish.shaurya.as@gmail.com>
2022-05-25 21:25:16 -0600

    Made JS better

--- sub.js
+++ sub.js
@@ -1,3 +1,15 @@
-function verify(plan){
-    
+let correct = "qGphJD"
+let successful = 0
+function verify(){
+    let givencaptcha = document.getElementById("givencaptcha").value
+    if(correct == givencaptcha){
+        setTimeout(function (){
+            location.href = "game-subscription.html"
+        },1000)
+        document.getElementById("incorrect").innerHTML =  "Correct Captcha!<br> Redirecting..."
+        document.getElementById("incorrect").style.color =  "green"
+    }
+    else{
+        document.getElementById("incorrect").innerText =  "Incorrect Captcha"
+    }
 }
