b6a1d3187681c2779540482e542b977f822efaf0
Shaurya-Sehgal <ashish.shaurya.as@gmail.com>
2022-05-29 15:26:47 -0600

    made better

--- openGame.js
+++ openGame.js
@@ -8,9 +8,6 @@ function opengame(game){
     else if(game == "gol"){
         location.href = "https://shaurya-sehgal.github.io/Game-Of-Luck/"
     }
-    else if(game == "ticTacToe") {
-        location.href = "https://shaurya-sehgal.github.io/Tic-Tac-Toe/"
-    }
     else if(game == "CarRace2"){
         location.href = "https://shaurya-sehgal.github.io/carrace2/"
     }
